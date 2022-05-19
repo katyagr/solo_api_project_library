3defaf0595771a89f28d855d4fe5df82813d6106
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 11:29:52 +0100

    added put methods

--- src/main/java/com/example/library_data/controller/AuthorController.java
+++ src/main/java/com/example/library_data/controller/AuthorController.java
@@ -3,10 +3,7 @@ package com.example.library_data.controller;
 import com.example.library_data.model.Author;
 import com.example.library_data.repository.AuthorRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,16 @@ public class AuthorController {
     public ResponseEntity<Author> addAuthor(@RequestBody Author author){
         Author newAuthor = authorRepository.save(author);
         return ResponseEntity.ok().body(newAuthor);
+    }
+
+    @PutMapping("authors/{id}")
+    public ResponseEntity<Author> updateAuthor(@RequestBody Author author, @PathVariable Long id){
+        Author update = authorRepository.findById(id).map(updatedAuthor -> {
+                    updatedAuthor.setFirstName(author.getFirstName());
+                    updatedAuthor.setLastName(author.getLastName());
+                    return authorRepository.save(updatedAuthor);})
+                .orElseGet(() -> {return authorRepository.save(author);});
+        return ResponseEntity.ok().body(update);
+
     }
 }
--- src/main/java/com/example/library_data/controller/BookController.java
+++ src/main/java/com/example/library_data/controller/BookController.java
@@ -3,10 +3,7 @@ package com.example.library_data.controller;
 import com.example.library_data.model.Book;
 import com.example.library_data.repository.BookRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,17 @@ public class BookController {
     public ResponseEntity<Book> addBook(@RequestBody Book book){
         Book newBook = bookRepository.save(book);
         return ResponseEntity.ok().body(newBook);
+    }
+
+    @PutMapping("books/{id}")
+    public ResponseEntity<Book> updateBook(@RequestBody Book book, @PathVariable Long id){
+        Book update = bookRepository.findById(id).map(updatedBook -> {
+            updatedBook.setTitle(book.getTitle());
+            updatedBook.setGenre(book.getGenre());
+            updatedBook.setAuthor(book.getAuthor());
+            return bookRepository.save(updatedBook);})
+                .orElseGet(() -> {return bookRepository.save(book);});
+        return ResponseEntity.ok().body(update);
+
     }
 }
