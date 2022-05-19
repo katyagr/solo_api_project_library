e617a15e8846d84fe4fd3a19b24d298b7173528c
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 10:37:29 +0100

    added getAll methods

--- src/main/java/com/example/library_data/controller/AuthorController.java
+++ src/main/java/com/example/library_data/controller/AuthorController.java
@@ -1,4 +1,25 @@
 package com.example.library_data.controller;
 
+import com.example.library_data.model.Author;
+import com.example.library_data.repository.AuthorRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AuthorController {
+
+    public final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @GetMapping("/authors")
+    public ResponseEntity<List<Author>> getAllAuthors(){
+        List<Author> authors = authorRepository.findAll();
+        return ResponseEntity.ok().body(authors);
+    }
 }
--- src/main/java/com/example/library_data/controller/BookController.java
+++ src/main/java/com/example/library_data/controller/BookController.java
@@ -1,4 +1,25 @@
 package com.example.library_data.controller;
 
+import com.example.library_data.model.Book;
+import com.example.library_data.repository.BookRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> books = bookRepository.findAll();
+        return ResponseEntity.ok().body(books);
+    }
 }
