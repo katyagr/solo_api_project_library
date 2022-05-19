a6185b7bbc1ef28d3c9795dfe227b4f02d858dc6
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 10:05:04 +0100

    made model classes

--- src/main/java/com/example/library_data/model/Author.java
+++ src/main/java/com/example/library_data/model/Author.java
@@ -1,4 +1,19 @@
 package com.example.library_data.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Book> books;
 }
--- src/main/java/com/example/library_data/model/Book.java
+++ src/main/java/com/example/library_data/model/Book.java
@@ -1,4 +1,15 @@
 package com.example.library_data.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String genre;
+    @ManyToOne
+    private Author author;
 }
