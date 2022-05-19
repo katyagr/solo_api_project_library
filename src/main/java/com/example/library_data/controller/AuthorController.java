380a6202bf52818c3c017e728a1f17761fa94ea2
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 10:48:28 +0100

    added post methods

--- src/main/java/com/example/library_data/controller/AuthorController.java
+++ src/main/java/com/example/library_data/controller/AuthorController.java
@@ -4,6 +4,8 @@ import com.example.library_data.model.Author;
 import com.example.library_data.repository.AuthorRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,11 @@ public class AuthorController {
     public ResponseEntity<List<Author>> getAllAuthors(){
         List<Author> authors = authorRepository.findAll();
         return ResponseEntity.ok().body(authors);
+    }
+
+    @PostMapping("/authors")
+    public ResponseEntity<Author> addAuthor(@RequestBody Author author){
+        Author newAuthor = authorRepository.save(author);
+        return ResponseEntity.ok().body(newAuthor);
     }
 }
--- src/main/java/com/example/library_data/controller/BookController.java
+++ src/main/java/com/example/library_data/controller/BookController.java
@@ -4,6 +4,8 @@ import com.example.library_data.model.Book;
 import com.example.library_data.repository.BookRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,11 @@ public class BookController {
     public ResponseEntity<List<Book>> getAllBooks(){
         List<Book> books = bookRepository.findAll();
         return ResponseEntity.ok().body(books);
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        Book newBook = bookRepository.save(book);
+        return ResponseEntity.ok().body(newBook);
     }
 }
