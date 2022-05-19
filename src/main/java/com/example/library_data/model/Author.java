b3e65ce277a559129443a3940f8664d8f8523619
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 10:07:40 +0100

    added constructors, getters/setters

--- src/main/java/com/example/library_data/model/Author.java
+++ src/main/java/com/example/library_data/model/Author.java
@@ -16,4 +16,46 @@ public class Author {
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Book> books;
+
+    public Author() {
+    }
+
+    public Author(Long id, String firstName, String lastName, List<Book> books) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/example/library_data/model/Book.java
+++ src/main/java/com/example/library_data/model/Book.java
@@ -12,4 +12,46 @@ public class Book {
     private String genre;
     @ManyToOne
     private Author author;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String genre, Author author) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
