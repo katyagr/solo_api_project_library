5b82bbdb0d93f5cac4db59d60fb0abea0e08da34
Katya Grenier <katyagrenier@hotmail.co.uk>
2022-05-19 14:50:42 +0100

    made tests for book repository pass

--- src/test/java/com/example/library_data/repository/BookRepositoryTest.java
+++ src/test/java/com/example/library_data/repository/BookRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.example.library_data.repository;
 
+import com.example.library_data.model.Book;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -13,11 +17,21 @@ class BookRepositoryTest {
 
     @Test
     void findByGenre_genreExists() {
-        fail();
+        //Given
+        String genre = "comedy";
+        //When
+        List<Book> booksOfGenre = bookRepository.findByGenre(genre);
+        //Then
+        assertTrue(StringUtils.containsIgnoreCase(booksOfGenre.get(0).getGenre(), genre));
     }
 
     @Test
     void findByGenre_genreDoesNotExist(){
-        fail();
+        //Given
+        String notAGenre = "upside down";
+        //When
+        List<Book> noBooks = bookRepository.findByGenre(notAGenre);
+        //Then
+        assertEquals(0, noBooks.size());
     }
 }
